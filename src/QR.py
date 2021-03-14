3fea64e30340c450150ff2904214e7d8b85613a8
Federico Finocchio <federico.fino@gmail.com>
2021-03-14 17:42:45 +0100

    Aggiunta funzione per thin QR factorization

--- src/QR.py
+++ src/QR.py
@@ -21,7 +21,31 @@ class QR:
         if x[0] > 0:
             n *= -1
 
-        v = x
+        v = np.copy(x)
         v[0] = v[0] - s
 
-        return (v/np.linalg.norm(v))  # pay attention here, it returns a numpy array
+        return n, (v/np.linalg.norm(v))  # pay attention here, it returns a numpy array
+
+
+    def qr(self, A):
+        """Computes the QR factorization for the given input matrix :param A: with dimensions m x n
+
+        :param A: Input matrix for which to computer the QR factorization
+        :returns: The triangular matrix R and a list of householder vectors used to compute the QR factorization
+        """
+
+        m, n = A.shape
+        Q = np.eye(m, n)
+        R = np.copy(A)
+        u_list = []
+
+        for j in range(np.min((m+1,n+1))):
+            s, u = self.householder_vector(R[j:,j])
+            u_list.append(u)
+
+            R[j, j] = s
+            R[j+1:, j] = 0
+            R[j:, j+1:] = R[j:, j+1:] - 2*u*(np.transpose(u) * R[j:, j+1:])
+            # Q[:, j:end] = Q[:, j:end] - Q[:, j:end]*u*2*np.transpose(u)  # Actually there is no need to return this matrix in the solving of LSP
+
+        return u_list, R
