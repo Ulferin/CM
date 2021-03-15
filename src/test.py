ec75d4294d78ebfef3d5d704c72a93755a5dca12
Federico Finocchio <federico.fino@gmail.com>
2021-03-15 16:28:42 +0100

    Aggiornato test

--- src/test.py
+++ src/test.py
@@ -1,16 +1,22 @@
-import QR
+from QR import QR
 import numpy as np
 import random
+from scipy.spatial import distance
+
+m = 1000
+n = 1000
 
 # Generate random matrix for QR decomposition
 def generate(m, n):
-    return np.array([ [random.gauss(0,1) for r in range(m)] for c in range(n) ])
+    M = np.array([ [random.gauss(0,1) for r in range(m)] for c in range(n) ])
+    b = np.array([random.gauss(0,1) for r in range(m)])
+    return M, b
 
 
 # task 2: use qr decomp for polynomial regression example
 def polyfit(qr, x, y, n):
     return lsqr(qr, x[:, None]**np.arange(n + 1), y.T)
- 
+
 
 def lsqr(qr, a, b):
     u_list, r = qr.qr(a)
@@ -19,23 +25,21 @@ def lsqr(qr, a, b):
     return np.dot( np.linalg.inv(r), implicit )
 
 
-# a = np.array(((
-#     (12, -51,   4),
-#     ( 6, 167, -68),
-#     (-4,  24, -41),
-# )))
+def solve(qr, M, b):
+    u_list, R = qr.qr(M)
+    _, n = R.shape
+    implicit = qr.implicit_Qb(b)[:n]
+    return np.dot( np.linalg.inv(R), implicit )
 
-m = 1000
-n = 1000
-
-qr = QR.QR()
-# u_list, R = qr.qr(M)
-# print(f"list of u: {u_list};\n\n R: {R}")
+qr = QR()
  
-x = np.array((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
-y = np.array((1, 6, 17, 34, 57, 86, 121, 162, 209, 262, 321))
-
-res = polyfit(qr, x, y, 2)
-
-print("\nreverse: ",np.dot(x[:, None]**np.arange(2 + 1), res))
+# x = np.array((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+# y = np.array((1, 6, 17, 34, 57, 86, 121, 162, 209, 262, 321))
+# res = polyfit(qr, x, y, 2)
+# print("\nreverse: ",np.dot(x[:, None]**np.arange(2 + 1), res))
 # print('\npolyfit:\n', res)
+
+M, b = generate(m, n)
+res = solve(qr, M, b)
+print(f"Distance is: {distance.cosine(np.dot(M, res), b)}")
+print("Done!")
