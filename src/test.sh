c6acb0fa27cde7aa028836b4e5072caac64179e6
Federico <federico@vmi534640.contaboserver.net>
2021-03-16 14:54:59 +0100

    Aggiunto script per testing

---
+++ src/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+m="$1"
+n="$2"
+step="$3"
+last="$4"
+
+# for i in $(seq $m $step $last); do for j in $(seq 500 500 $i); do python test.py $i $j >> test.txt; done; done;
+for i in $(seq $m $step $last)
+do
+	for j in $(seq $n $step $i)
+	do
+		echo "$i $j"
+		python test.py $i $j >> test_aux.txt
+	done
+done
