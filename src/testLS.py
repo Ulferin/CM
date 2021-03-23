dd4830ca75602436043e6fa2a7dccbc496a9e805
Federico Finocchio <federico.fino@gmail.com>
2021-03-23 16:58:07 +0100

    Refactoring test

--- src/testLS.py
+++ src/testLS.py
@@ -6,9 +6,8 @@ import time
 
 from datetime import datetime as dt
 
-
-m = int(sys.argv[1])
-n = int(sys.argv[2])
+CUP_TEST = 'cup'
+RANDOM_TEST = 'random'
 
 
 def generate(m, n):
@@ -43,92 +42,110 @@ def load_ML_CUP_dataset ( filename ):
     :return: The coefficient matrix M and the dependend variables vector b.
     """
 	
-	M = []
-	b = []
-	
-	with open (filename) as fin:
-		for line in fin:
-			if not line=="\n" and not line.startswith ("#"): 
-				line_split = line.split(",")
-				row = [ float(x) for x in line_split[1:-2] ]
-				b_el = float(line_split[-1])
-				
-				M.append (row)
-				b.append (b_el)
-	
-	return np.array(M), np.array(b)
+    M = []
+    b = []
+
+    with open (filename) as fin:
+        for line in fin:
+            if not line=="\n" and not line.startswith ("#"): 
+                line_split = line.split(",")
+                row = [ float(x) for x in line_split[1:-2] ]
+                b_el = float(line_split[-1])
+                
+                M.append (row)
+                b.append (b_el)
+
+    return np.array(M), np.array(b)
 
 
+def test_random_dataset(m, n):
+    # ---------- TEST ON RANDOM DATASET ----------
+    ls = LS()
+    M, b = generate(m, n)
 
-# ---------- TEST ON RANDOM DATASET ----------
-ls = LS()
-M, b = generate(m, n)
+    # Computes time for LS solver 
+    startLS = dt.now()
+    res = ls.solve(M,b)
+    endLS = end_time(startLS)
 
-# Computes time for LS solver 
-startLS = dt.now()
-res = ls.solve(M,b)
-endLS = end_time(startLS)
-
-# Computes time for Q and QR reconstruction
-startQR = dt.now()
-R = ls.qr(M)
-Q = ls.revertQ()
-R_complete = np.zeros((m,n))
-R_complete[:n, :n] = R_complete[:n, :n] + R
-QR = np.dot(Q, R_complete)
-endQR = end_time(startQR)
-
-
-# Computes QR factorization using numpy
-startQRnp = dt.now()
-Qnp, Rnp = np.linalg.qr(M, mode="complete")
-QRnp = np.dot(Qnp, Rnp)
-endQRnp = end_time(startQRnp)
-
-# Computes time for LS solver using numpy
-startLSnp = dt.now()
-resnp, _, _, _ = np.linalg.lstsq(M,b,rcond=-1)
-endLSnp = end_time(startLSnp)
-# ---------- TEST ON RANDOM DATASET ----------
-
-print(f"---------- RANDOM DATASET ----------")
-print(f"Solved (m x n): ({m},{n}) in {endLS} msec, w/ np in {endLSnp} msec \
-- Reverting and reconstruction: {endQR} msec, w/ np took: {endQRnp} msec")
-print(f"res error: {np.linalg.norm( b - np.dot(M, res) )/np.linalg.norm(b)} \
-- np_res error: {np.linalg.norm( b - np.dot(M, resnp) )/np.linalg.norm(b)}")
-print(f"QR error: {np.linalg.norm( M - QR )/np.linalg.norm(QR)} \
-- QR error w/ np: {np.linalg.norm( M - QRnp )/np.linalg.norm(QRnp)}\n")
+    # Computes time for Q and QR reconstruction
+    startQR = dt.now()
+    R = ls.qr(M)
+    Q = ls.revertQ()
+    R_complete = np.zeros((m,n))
+    R_complete[:n, :n] = R_complete[:n, :n] + R
+    QR = np.dot(Q, R_complete)
+    endQR = end_time(startQR)
 
 
-# ---------- TEST ON CUP DATASET ----------
-M, b = load_ML_CUP_dataset("../data/ML-CUP18-TR.csv")
+    # Computes QR factorization using numpy
+    startQRnp = dt.now()
+    Qnp, Rnp = np.linalg.qr(M, mode="complete")
+    QRnp = np.dot(Qnp, Rnp)
+    endQRnp = end_time(startQRnp)
 
-# Computes time for LS solver 
-startLS = dt.now()
-res = ls.solve(M,b)
+    # Computes time for LS solver using numpy
+    startLSnp = dt.now()
+    resnp, _, _, _ = np.linalg.lstsq(M,b,rcond=-1)
+    endLSnp = end_time(startLSnp)
 
-R = ls.qr(M)
-Q = ls.revertQ()
-R_complete = np.zeros((m,n))
-R_complete[:n, :n] = R_complete[:n, :n] + R
-QR = np.dot(Q, R_complete)
+    print(f"---------- RANDOM DATASET ----------")
+    print(f"Solved (m x n): ({m},{n}) in {endLS} msec, w/ np in {endLSnp} msec \
+    - Reverting and reconstruction: {endQR} msec, w/ np took: {endQRnp} msec")
+    print(f"res error: {np.linalg.norm( b - np.dot(M, res) )/np.linalg.norm(b)} \
+    - np_res error: {np.linalg.norm( b - np.dot(M, resnp) )/np.linalg.norm(b)}")
+    print(f"QR error: {np.linalg.norm( M - QR )/np.linalg.norm(QR)} \
+    - QR error w/ np: {np.linalg.norm( M - QRnp )/np.linalg.norm(QRnp)}\n")
 
-endLS = end_time(startLS)
+    # ---------- TEST ON RANDOM DATASET ----------
 
-# Computes time for LS solver using numpy
-startLSnp = dt.now()
-resnp, _, _, _ = np.linalg.lstsq(M,b,rcond=-1)
 
-Qnp, Rnp = np.linalg.qr(M, mode="complete")
-QRnp = np.dot(Qnp, Rnp)
+def test_cup():
+    # ---------- TEST ON CUP DATASET ----------
+    ls = LS()
+    M, b = load_ML_CUP_dataset("../data/ML-CUP18-TR.csv")
 
-endLSnp = end_time(startLSnp)
+    m = len(M)
+    n = len(M[0])
 
-print(f"---------- CUP DATASET ----------")
-print(f"Solved (m x n): ({len(M)},{len(M[0])}) in {endLS} msec, w/ np in {endLSnp} msec")
-print(f"res error: {np.linalg.norm( b - np.dot(M, res) )/np.linalg.norm(b)} \
-- np_res error: {np.linalg.norm( b - np.dot(M, resnp) )/np.linalg.norm(b)}")
-print(f"QR error: {np.linalg.norm( M - QR )/np.linalg.norm(QR)} \
-- QR error w/ np: {np.linalg.norm( M - QRnp )/np.linalg.norm(QRnp)}\n")
+    # Computes time for LS solver 
+    startLS = dt.now()
+    res = ls.solve(M,b)
 
-# ---------- TEST ON CUP DATASET ----------
+    R = ls.qr(M)
+    Q = ls.revertQ()
+    R_complete = np.zeros((m,n))
+    R_complete[:n, :n] = R_complete[:n, :n] + R
+    QR = np.dot(Q, R_complete)
+
+    endLS = end_time(startLS)
+
+    # Computes time for LS solver using numpy
+    startLSnp = dt.now()
+    resnp, _, _, _ = np.linalg.lstsq(M,b,rcond=-1)
+
+    Qnp, Rnp = np.linalg.qr(M, mode="complete")
+    QRnp = np.dot(Qnp, Rnp)
+
+    endLSnp = end_time(startLSnp)
+
+    print(f"---------- CUP DATASET ----------")
+    print(f"Solved (m x n): ({len(M)},{len(M[0])}) in {endLS} msec, w/ np in {endLSnp} msec")
+    print(f"res error: {np.linalg.norm( b - np.dot(M, res) )/np.linalg.norm(b)} \
+    - np_res error: {np.linalg.norm( b - np.dot(M, resnp) )/np.linalg.norm(b)}")
+    print(f"QR error: {np.linalg.norm( M - QR )/np.linalg.norm(QR)} \
+    - QR error w/ np: {np.linalg.norm( M - QRnp )/np.linalg.norm(QRnp)}\n")
+
+    # ---------- TEST ON CUP DATASET ----------
+
+
+if __name__ == "__main__":
+    test = sys.argv[1]      # test type ('cup' or 'random')
+
+    if test == CUP_TEST:
+        test_cup()
+    elif test == RANDOM_TEST:
+        assert len(sys.argv) == 4, "This kind of test requires 'm' and 'n' dimensions to be defined."
+        m = int(sys.argv[2])    # number of rows
+        n = int(sys.argv[3])    # number of cols
+        test_random_dataset(m, n)
