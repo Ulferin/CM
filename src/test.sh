cbe4523068f61ee7a999a728f6b6878d3b4e1698
Federico Finocchio <federico@vmi534640.contaboserver.net>
2021-03-23 10:47:43 +0100

    Improved testing script

--- src/test.sh
+++ src/test.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-m="$1"
-n="$2"
-stepm="$3"
-stepn="$4"
-lastm="$5"
-lastn="$6"
+m="$1"		# starting value for m
+n="$2"		# starting value for n
+stepm="$3"	# step for m
+stepn="$4"	# step for n
+lastm="$5"	# last value for m
+lastn="$6"	# last value for n
 
-# for i in $(seq $m $step $last); do for j in $(seq 500 500 $i); do python test.py $i $j >> test.txt; done; done;
+# for each value of m from 'm' to 'lastm' with step 'stepm'
 for i in $(seq $m $stepm $lastm)
 do
+	# for each value of n from 'n' to 'lastn' with step 'lastn'
 	for j in $(seq $n $stepn $lastn)
 	do
-		python test.py $i $j >> test_script.txt
+		python testLS.py $i $j >> "test${lastm}_${lastn}.txt"
 	done
 done
