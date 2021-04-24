6a4273b9eb098bc8e17d5c661b78c7163729fea7
Federico Finocchio <federico.fino@gmail.com>
2021-04-24 08:55:40 +0200

    Maybe we are solving

--- src/NN/testNN.py
+++ src/NN/testNN.py
@@ -10,8 +10,8 @@ input_units = X_train.shape[1]
 output_units = y_train.shape[1]
 
 # Builds the training data for the NN
-training_data = [ (x.reshape(-1,1),y) for x,y in zip(X_train, y_train)]
-test_data = [ (x,y) for x,y in zip(X_test, y_test)]
+training_data = [ (x.reshape(1,-1),y) for x,y in zip(X_train, y_train)]
+test_data = [ (x.reshape(1,-1),y) for x,y in zip(X_test, y_test)]
 
 net = Network([input_units, 2, 4, output_units], 0)
 net.SGD(training_data[:1], 100, 50, 1, test_data)
