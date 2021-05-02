1a0e880a9c9899565c1358b1522a7fe43fd8f8d7
Federico Finocchio <federico.fino@gmail.com>
2021-05-02 17:16:40 +0200

    Going to bugfix this shit

--- src/NN/testCUP.py
+++ src/NN/testCUP.py
@@ -31,7 +31,7 @@ eta = [0.1, 0.2, 0.5, 1, 2]
 #                     net.SGD(training_data, ep, b, e, test_data)
 #                     net.best_score()
 
-net = NR([input_units, 5, 5, output_units], 0, 'relu2', lmbda=0.1, momentum=0.5, debug=True)
-net.SGD(training_data, epochs=1000, batch_size=10, eta=0.001, test_data=test_data)
+net = NR([input_units, 2, output_units], 0, 'relu', lmbda=0.001, momentum=0.9, debug=True)
+net.SGD(training_data, epochs=1000, batch_size=10, eta=0.01, test_data=test_data)
 print(net.best_score())
-net.plot_score(f"CUP/cup")
+# net.plot_score(f"CUP/cup")
