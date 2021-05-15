71a626eee1479af3f8f9ca558baa1cd8162e9835
Federico Finocchio <federico.fino@gmail.com>
2021-05-15 09:50:19 +0200

    Object Oriented version of activation function

--- src/NN/functions.py
+++ src/NN/functions.py
@@ -1,49 +1,58 @@
 import numpy as np
+from abc import ABCMeta, abstractmethod
 
-def relu(x):
-    """Utility function that implements the ReLU function used in the NN units.
-    It works both with vectors and scalars. 
+class ActivationFunction(metaclass=ABCMeta):
 
-    :param x: input vector/scalar
-    :return: x if x >= 0, 0 otherwise
-    """    
-    return np.maximum(x, 0)
+    @abstractmethod
+    def function(self, x):
+        pass
 
-
-def relu_prime(x):
-    """Utility function that implements the ReLU function derivative.
-    It works both with vectors and scalars.
-
-    :param x: input vector/scalar
-    :return: 1 if :param x: is greater than 0, 0 otherwise
-    """    
-    # TODO: problema quando x è zero!!!
-    return np.maximum(x, 0)/x
-
-def ReLU(x):
-    return x * (x > 0)
-
-def dReLU(x):
-    return 1. * (x > 0)
-
-def sigmoid(z):
-    """Function that defines the sigmoid activation function used in the
-    Neaural Network units. It works both with vectors and numbers.
-
-    :param z: element to which apply the sigmoid function.
-    :return: an element with the same shape of the input with
-             sigmoid function applied elementwise in case of a vector.
-    """
-
-    return 1.0 / (1.0 + np.exp(-z))
+    @abstractmethod
+    def derivative(self, x):
+        pass
 
 
-def sigmoid_prime(z):
-    """Derivative function of the sigmoid function.
+class ReLU(ActivationFunction):
+    
+    def function(self, x):
+        """Utility function that implements the ReLU function used in the NN units.
+        It works both with vectors and scalars. 
 
-    :param z: element to which apply the sigmoid derivative function.
-    :return: an element with the same shape of the input with
-             sigmoid derivative function applied elementwise in case of a vector.
-    """
+        :param x: input vector/scalar
+        :return: x if x >= 0, 0 otherwise
+        """
+        return np.maximum(x, 0.)
 
-    return sigmoid(z) * (1 - sigmoid(z))
+    def derivative(self, x):
+        """Utility function that implements the ReLU function derivative.
+        It works both with vectors and scalars.
+
+        :param x: input vector/scalar
+        :return: 1 if :param x: is greater than 0, 0 otherwise
+        """    
+        return 1. * (x > 0)
+
+
+class Sigmoid(ActivationFunction):
+
+    def function(self, x):
+        """Function that defines the sigmoid activation function used in the
+        Neaural Network units. It works both with vectors and numbers.
+
+        :param z: element to which apply the sigmoid function.
+        :return: an element with the same shape of the input with
+                sigmoid function applied elementwise in case of a vector.
+        """
+
+        return 1.0 / (1.0 + np.exp(-x))
+
+
+    def derivative(self, x):
+        """Derivative function of the sigmoid function.
+
+        :param z: element to which apply the sigmoid derivative function.
+        :return: an element with the same shape of the input with
+                sigmoid derivative function applied elementwise in case of a vector.
+        """
+        x = self.function(x)
+        return x * (1 - x)
