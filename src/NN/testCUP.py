303008f49182dce48e029046d4875fe0a18d712b
Federico Finocchio <federico.fino@gmail.com>
2021-05-16 18:02:54 +0200

    Updated cup data loading

--- src/NN/testCUP.py
+++ src/NN/testCUP.py
@@ -29,8 +29,8 @@ momentum = [0.5, 0.9]
 if __name__ == '__main__':
     test = sys.argv[1]
 
-    h1 = 20
-    h2 = 16
+    h1 = 5
+    h2 = 3
     activation = 'relu'
     lmbda = 0.0001
     momentum = 0.9
@@ -52,7 +52,7 @@ if __name__ == '__main__':
                                     net.plot_score(f"test_np/cup")
 
     elif test == 'std':
-        net = NR([input_units, h1, h2, output_units], 0, activation, lmbda=lmbda, momentum=momentum, debug=True)
+        net = NR([input_units, h1, output_units], 0, activation, lmbda=lmbda, momentum=momentum, debug=True)
         net.SGD((training_data, y_train), epochs=epochs, batch_size=batch_size, eta=eta, test_data=(test_data, y_test))
         print(f"The best score for ep:{epochs}, h1:{h1}, h2:{h2}, b:{batch_size}, e:{eta}, l:{lmbda}, m:{momentum} was: {net.best_score()}")
         net.plot_score(f"test_np/cup")
--- src/NN/utils.py
+++ src/NN/utils.py
@@ -1,12 +1,14 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 
 def load_CUP(name):
     df = pd.read_csv(name, header=None, index_col=0, skiprows=7)
-    M = df.iloc[:,:-2]
-    b = df.iloc[:,-2:]
+
+    ml_cup = np.delete(np.genfromtxt(name, delimiter=','), obj=0, axis=1)
+    M, b = ml_cup[:, :-2], ml_cup[:, -2:]
 
     X_train, X_test, y_train, y_test = train_test_split(
-            M.values, b.values, test_size=0.3, random_state=0)
+            M, b, test_size=0.3, random_state=0)
 
     return X_train, X_test, y_train, y_test
