c1fe6a82190a78c96a23c15124a7e079e26d41e1
Federico Finocchio <federico.fino@gmail.com>
2021-07-08 14:34:54 +0200

    Added batch version

--- src/NN/NC.py
+++ src/NN/NC.py
@@ -24,7 +24,7 @@ class NC(Network):
         return best_score
 
 
-    def evaluate(self, test_data, train_data):
+    def evaluate(self, test_data, train_data, batch=False):
         """Evaluates the performances of the Network in the current state,
         propagating the test examples through the network via a complete feedforward
         step. It evaluates the performance using the R2 metric in order to be
@@ -37,11 +37,19 @@ class NC(Network):
         score_test = []
         score_train = []
 
-        preds_test = [ np.array(self.feedforward(x)[2] >= 0.5).reshape(y.shape) for x,y in zip(test_data[0], test_data[1])]
-        truth_test = [ y for y in test_data[1]]
+        if batch:
+            preds_test = self.feedforward_batch(test_data[0])[2] >= 0.5
+            truth_test = test_data[1]
 
-        preds_train = [np.array(self.feedforward(x)[2] >= 0.5).reshape(y.shape) for x,y in zip(train_data[0], test_data[1])]
-        truth_train = [y for y in train_data[1]]
+            preds_train = self.feedforward_batch(train_data[0])[2] >= 0.5
+            truth_train = train_data[1]
+
+        else:
+            preds_test = [ np.array(self.feedforward(x)[2] >= 0.5).reshape(y.shape) for x,y in zip(test_data[0], test_data[1])]
+            truth_test = [ y for y in test_data[1]]
+
+            preds_train = [np.array(self.feedforward(x)[2] >= 0.5).reshape(y.shape) for x,y in zip(train_data[0], test_data[1])]
+            truth_train = [y for y in train_data[1]]
 
         score_test.append(accuracy_score(truth_test, preds_test))
         score_train.append(accuracy_score(truth_train, preds_train))
