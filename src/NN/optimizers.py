1493961522853ceec52f2540cff9f19a23b2cf64
Federico Finocchio <federico@vmi534640.contaboserver.net>
2021-08-07 15:09:10 +0200

    Added docstrings

--- src/NN/optimizers.py
+++ src/NN/optimizers.py
@@ -7,13 +7,15 @@ class Optimizer(metaclass=ABCMeta):
 
     @abstractmethod
     def __init__(self, eta, eps=1e-5):
-        """Initialize the optimizer with the specified learning rate :eta: and the
-        desire precision :eps:.
+        """Initialize the optimizer with the specified learning rate :eta:
+        and the desire precision :eps:.
 
         Parameters
         ----------
         eta : float
-            Learning rate for SGD optimizer or starting step size for SGM optimizer
+            Learning rate for SGD optimizer or starting step size for SGM
+            optimizer
+        
         eps : float, optional
             Desired precision for gradient norm, by default 1e-5
         """        
@@ -23,17 +25,7 @@ class Optimizer(metaclass=ABCMeta):
 
 
     @abstractmethod
-    def update_mini_batch(self, nn, mini_batch):
-        """Updates weights and biases of the specified Neural Network object :nn: by
-        using the current mini-batch samples :mini_batch:.
-
-        Parameters
-        ----------
-        nn : Object
-            Neural Network to use for updates and gradient computation.
-        mini_batch : np.ndarray
-            mini-batch samples to use for updates.
-        """              
+    def update_mini_batch(self, nn, mini_batch):           
         pass
 
 
@@ -57,35 +49,46 @@ class SGD(Optimizer):
 
 
     def update_mini_batch(self, nn, mini_batch):
-        """Updates weights and biases of the specified Neural Network object :nn: by
-        using the current mini-batch samples :mini_batch:. Uses a regularized momentum
-        based approach for the weights update. Hyperparameters must be configured directly
-        on the :nn: object.
+        """Updates weights and biases of the specified Neural Network object
+        :nn: by using the current mini-batch samples :mini_batch:. Uses a
+        regularized momentum based approach for the weights update.
+        Hyperparameters must be configured directly on the :nn: object.
 
         Parameters
         ----------
         nn : Object
             Neural Network to use for updates and gradient computation.
+        
         mini_batch : np.ndarray
             mini-batch samples to use for updates.
         """
         size = len(mini_batch[0])
 
+        # Nesterov update
         if nn.nesterov:
-            # Nesterov update
-            nn.weights = [w + nn.momentum*wv for w, wv in zip(nn.weights, nn.wvelocities)]
-            nn.biases = [b + nn.momentum*bv for b, bv in zip(nn.biases, nn.bvelocities)]
+            nn.weights = [w + nn.momentum * wv
+                            for w, wv in zip(nn.weights, nn.wvelocities)]
+            nn.biases = [b + nn.momentum * bv
+                            for b, bv in zip(nn.biases, nn.bvelocities)]
 
         # Compute current gradient
         nabla_b, nabla_w = nn._compute_grad(mini_batch)
 
         # Updates velocities with the current momentum coefficient
-        nn.wvelocities = [nn.momentum * velocity - (self.eta/size)*nw for velocity,nw in zip(nn.wvelocities, nabla_w)]
-        nn.bvelocities = [nn.momentum * velocity - (self.eta/size)*nb for velocity,nb in zip(nn.bvelocities, nabla_b)]
+        nn.wvelocities = [nn.momentum * velocity - (self.eta/size)*nw
+                          for velocity,nw 
+                          in zip(nn.wvelocities, nabla_w)]
+        nn.bvelocities = [nn.momentum * velocity - (self.eta/size)*nb
+                          for velocity,nb
+                          in zip(nn.bvelocities, nabla_b)]
 
         # Updates weights
-        nn.weights = [w + velocity - (nn.lmbda/size) * w for w,velocity in zip(nn.weights, nn.wvelocities)]
-        nn.biases = [b + velocity for b,velocity in zip(nn.biases, nn.bvelocities)]
+        nn.weights = [w + velocity - (nn.lmbda/size) * w
+                      for w, velocity
+                      in zip(nn.weights, nn.wvelocities)]
+        nn.biases = [b + velocity
+                     for b, velocity
+                     in zip(nn.biases, nn.bvelocities)]
 
     def iteration_end(self, nn):
         """Checks if the optimizer has reached an optimal state.
@@ -124,15 +127,16 @@ class SGM(Optimizer):
 
 
     def update_mini_batch(self, nn, mini_batch):
-        """Updates weights and biases of the specified Neural Network object :nn: by
-        using the current mini-batch samples :mini_batch:. Uses a diminishing square summable
-        step size as specified by AdaGrad. Hyperparameters must be configured directly
-        on the :nn: object.
+        """Updates weights and biases of the specified Neural Network object
+        :nn: by using the current mini-batch samples :mini_batch:. Uses a
+        diminishing square summable step size as specified by AdaGrad.
+        Hyperparameters must be configured directly on the :nn: object.
 
         Parameters
         ----------
         nn : Object
             Neural Network to use for updates and gradient computation.
+        
         mini_batch : np.ndarray
             mini-batch samples to use for updates.
         """       
@@ -149,11 +153,15 @@ class SGM(Optimizer):
         self.gms_b = [gb + nb**2 for gb, nb in zip(self.gms_b, nabla_b)]
         self.gms_w = [gw + nw**2 for gw, nw in zip(self.gms_w, nabla_w)]
 
-        nabla_b = [nb/(np.sqrt(gms_b)+self.offset) for nb, gms_b in zip(nabla_b, self.gms_b)]
-        nabla_w = [nw/(np.sqrt(gms_w)+self.offset) for nw, gms_w in zip(nabla_w, self.gms_w)]
+        nabla_b = [nb / (np.sqrt(gms_b) + self.offset)
+                   for nb, gms_b in zip(nabla_b, self.gms_b)]
+        nabla_w = [nw / (np.sqrt(gms_w) + self.offset)
+                   for nw, gms_w in zip(nabla_w, self.gms_w)]
 
-        nn.weights = [w - self.step*nw - (nn.lmbda/size) * w for w,nw in zip(nn.weights, nabla_w)]
-        nn.biases = [b - self.step*nb for b,nb in zip(nn.biases, nabla_b)]
+        nn.weights = [w - self.step*nw - (nn.lmbda/size) * w
+                      for w, nw in zip(nn.weights, nabla_w)]
+        nn.biases = [b - self.step*nb
+                     for b, nb in zip(nn.biases, nabla_b)]
 
 
     def iteration_end(self, nn):      
