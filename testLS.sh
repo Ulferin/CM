3ce20ac207d45d66e49a38703df960a222e7b023
Federico Finocchio <federico@vmi534640.contaboserver.net>
2021-08-09 18:00:27 +0200

    Created first script for testing LS

--- testLS.sh
+++ testLS.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-m="$1"		# starting value for m
-n="$2"		# starting value for n
-stepm="$3"	# step for m
-lastm="$4"	# last value for m
+t="$1"		# test type
+m="$2"		# starting value for m
+n="$3"		# starting value for n
+stepm="$4"	# step for m
+lastm="$5"	# last value for m
 
-# for each value of m from 'm' to 'lastm' with step 'stepm'
-for i in $(seq $m $stepm $lastm)
-do
-	python -m src.LS.testLS 'RANDOM' $i $n >> "test${lastm}_${n}.txt"
-done
+REPEAT=10
+
+# TODO: cambiare tipo di test in base a valore di t:
+#		creare test per scaling, test per random e test per CUP
+case $t in
+	'RANDOM')
+		# for each value of m from 'm' to 'lastm' with step 'stepm'
+		python -m src.LS.testLS 'RANDOM' $m $n >> "test_random${m}_${n}.txt"
+	;;
+	'SCALING')
+		python -m src.LS.testLS 'SCALING' $m $lastm $n $stepm $REPEAT >> "test_scaling${lastm}_${n}.txt"
+	;;
+esac
