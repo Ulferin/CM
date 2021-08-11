d68a088226d84b13cd1876a8d377fb0ee9f5c340
Federico Finocchio <federico@vmi534640.contaboserver.net>
2021-08-11 15:33:32 +0200

    Modified scripts for testing; Added instruction to run

--- testLS.sh
+++ testLS.sh
@@ -1,13 +1,50 @@
 #!/bin/bash
 
+# Author: Federico Finocchio -- ID: 516818
+# email: f.finocchio@studenti.unipi.it
+# AA: 2020/2021
+
+# Script file to test the execution of the QR factorization and LS solver.
+# Refers to the implementation of the LS class in src/LS/LS.py which is used to
+# solve the Least Square problem of the form  min ||Ax - b||.
+
+# NOTE: all the results for the tests performed using this script file will be
+#		printed to a specific file with a standard name relative to the performed
+#		test. 
+# To perform the test just run:
+# ./testLS.sh 't' ['m' 'n' 'stepm' 'lastm']
+
+# Where:
+#	· t: test type to perform:
+#		- 'RANDOM': to test the execution of the two algorithm over a randomly
+#					generated set of data with dimensions as specified by the
+#					parameters 'm' and 'n', that represents, respectively,
+#					the number of rows and columns of the matrix A ;
+#
+#		- 'SCALING': to test the performance of the two algorithms over increasing
+#					 dimension 'm' with a fixed 'n'. Starting from 'm' to 'lastm'
+#					 with a 'stepm' increment, test each of the randomly generated
+#					 datasets up to A in R^[lastm x n] ;
+#
+#		- 'CUP':  to test the two algorithms over the dataset provided for the ML
+#				  cup, as specified in the report file.
+#
+#	· m: number of rows for the 'RANDOM' test and starting number of rows for
+#		 'SCALING' test. Must be greater than 0;
+#
+#	· n: number of columns for the matrix A;
+#
+#	· stepm: increment for the m dimension, to be used only for 'SCALING' test;
+#
+#	· lastm: last value for the m dimension, to be used only for 'SCALING' test;
+
+
 t="$1"		# test type
 m="$2"		# starting value for m
 n="$3"		# starting value for n
 
 REPEAT=10
 
-# TODO: cambiare tipo di test in base a valore di t:
-#		creare test per scaling, test per random e test per CUP
 case $t in
 	'RANDOM')
 		# for each value of m from 'm' to 'lastm' with step 'stepm'
@@ -19,5 +56,5 @@ case $t in
 		python -m src.LS.testLS $t $m $lastm $n $stepm $REPEAT >> "test_scaling${lastm}_${n}.txt"
 	;;
 	'CUP')
-		python -m src.LS.testLS $t 'data/ML-CUP20-TR.csv' >> "test_cup_LS.txt"
+		python -m src.LS.testLS $t 'data/ML-CUP20-TR.csv' >> "test_cup.txt"
 esac
--- testNN.sh
+++ testNN.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
+# Author: Federico Finocchio -- ID: 516818
+# email: f.finocchio@studenti.unipi.it
+# AA: 2020/2021
+
+# Script file to test the execution of the QR factorization and LS solver.
+# Refers to the implementation of the LS class in src/LS/LS.py which is used to
+# solve the Least Square problem of the form  min ||Ax - b||.
+
+# NOTE: all the results for the tests performed using this script file will be
+#		printed to a specific file with a standard name relative to the performed
+#		test. 
+# To perform the test just run:
+# ./testNN.sh 'dataset' 'optimizer' 'grid'
+
+# Where:
+#	· dataset: dataset to use for the test, it can be 'monk1', 'monk2', 'monk3',
+#             'cup'. It will use the associated dataset as specified in the report
+#              file;
+#
+#	· optimizer: optimizer to use for the speficied test, either 'SGD' or 'SGM';
+#
+#
+#	· grid: whether to perform a grid search over the specified dataset/optimizer.
+#           Either true or false.
+
+
 dataset="$1"
 optimizer="$2"
-t="$3"
+grid="$3"
 
-GRID="GRID"
-
-case $t in
-    $GRID)
-        python -m src.NN.testNET $optimizer $dataset 'grid' >> "test_grid_${dataset}_${optimizer}.txt"
-    ;;
-    *)
-        python -m src.NN.testNET $optimizer $dataset >> "test_${dataset}_${optimizer}.txt"
-    ;;
-esac
+if $grid
+then
+    python -m src.NN.testNET $optimizer $dataset 'grid' >> "test_grid_${dataset}_${optimizer}.txt"
+else
+    python -m src.NN.testNET $optimizer $dataset >> "test_${dataset}_${optimizer}.txt"
+fi
